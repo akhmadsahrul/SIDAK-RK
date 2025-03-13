0b66ced0dd66a8383cbe577bdd64ce05706f4988
akhmadsahrul099 <akhmadsahrul099@gmail.com>
2025-03-13 11:47:50 +0700

    progres1

--- sidak-rk/sidak_app/urls.py
+++ sidak-rk/sidak_app/urls.py
@@ -10,10 +10,18 @@ urlpatterns = [
     path('reset-password/', views.resetpassword, name='resetpassword'), 
     path('new-password/', views.setnew, name='setnew'), 
 
+    # Ubah Kata sandi profile
+    path('ubah-sandi/', views.ubah_katasandi, name='ubah_katasandi'),
+
     # admin
     path('dashboard-admin/', views.dashboard_admin, name='dashboard_admin'), 
     path('data-ajuan/', views.data_ajuan, name='data_ajuan'), 
+    path('data-karyawan/', views.data_karyawan, name='data_karyawan'), 
+    path('edit-data-karyawan/', views.edit_data_karyawan, name='edit_data_karyawan'), 
+    path('detail-data-karyawan/', views.detail_data_karyawan, name='detail_data_karyawan'), 
     path('history/', views.history, name='history'),
+    path('dataaccount/', views.data_akun, name='data_akun'),
+    
 
     # karyawan
     path('dashboard/', views.dashboard_karyawan, name='dashboard_karyawan'),
--- sidak-rk/sidak_app/views.py
+++ sidak-rk/sidak_app/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 # Login
 def login(request):
     return render(request, 'login.html')
+
+# Login
+def ubah_katasandi(request):
+    return render(request, 'ubah_kata_sandi.html')
+
 # Forgot Password
 def forgotpassword(request):
     return render(request, 'forgotpassword.html')
@@ -19,8 +24,16 @@ def dashboard_admin(request):
     return render(request, 'views/admin/dashboard_admin.html')
 def data_ajuan(request):
     return render(request, 'views/admin/data_ajuan.html')
+def data_karyawan(request):
+    return render(request, 'views/admin/data_karyawan.html')
+def edit_data_karyawan(request):
+    return render(request, 'views/admin/edit_data_karyawan.html')
+def detail_data_karyawan(request):
+    return render(request, 'views/admin/detail_data_karyawan.html')
 def history(request):
     return render(request, 'views/admin/history.html')
+def data_akun(request):
+    return render(request, 'views/admin/data_akun.html')
 
 # karyawan
 def dashboard_karyawan(request):
